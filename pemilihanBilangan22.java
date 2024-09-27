b0c6a2075e09d54f1be5db008ddd7b8ad1604e8f
Okta Pratama <oktaiwp849@gmail.com>
2024-09-27 08:42:36 +0700

    CommitBilanganPertanyaan

--- pemilihanBilangan22.java
+++ pemilihanBilangan22.java
@@ -7,5 +7,7 @@ public class pemilihanBilangan22 {
         int angka = sc.nextInt();
         String hasil = (angka % 2 == 0) ? "bilangan genap" : "bilangan ganjil";
         System.out.println("Angka " + angka + " termasuk " + hasil);
+
+        sc.close();
     }
 }
