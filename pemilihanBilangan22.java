2bbd3d48cfe1a898436d3502c43ba3a746b71eb5
Okta Pratama <oktaiwp849@gmail.com>
2024-09-27 09:41:14 +0700

    pemilihanHari

--- pemilihanBilangan22.java
+++ pemilihanBilangan22.java
@@ -5,6 +5,7 @@ public class pemilihanBilangan22 {
         Scanner sc = new Scanner(System.in);
         System.out.print("masukkan sebuah angka : ");
         int angka = sc.nextInt();
+        
         String hasil = (angka % 2 == 0) ? "bilangan genap" : "bilangan ganjil";
         System.out.println("Angka " + angka + " termasuk " + hasil);
 
