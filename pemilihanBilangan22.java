fc1c9bcaaa95e12ebef4c8b404b51853220cce84
Okta Pratama <oktaiwp849@gmail.com>
2024-09-27 08:00:50 +0700

    pemilihanBilangan

---
+++ pemilihanBilangan22.java
@@ -0,0 +1,19 @@
+package praktikum5;
+import java.util.Scanner;
+
+public class pemilihanBilangan22 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("masukkan sebuah angka : ");
+        int angka = sc.nextInt();
+
+        if (angka % 2 == 0){
+            System.out.println( "angka " + angka + " termasuk bilangan genap");
+        }else{
+            System.out.println("Angka " + angka + " termasuk bilangan ganjil");
+        }
+    }
+    
+}
